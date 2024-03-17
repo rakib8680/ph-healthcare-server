55c96e5f8cfa6649048fb84ff5919ca20a7921d5
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-17 13:04:50 +0600

    Refactor user controller to handle admin creation and retrieval with error handling

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,15 +3,28 @@ import { userServices } from "./user.service";
 
 // create admin
 const createAdmin: RequestHandler = async (req, res) => {
-  const result = await userServices.createAdminIntoDB(req.body);
-
-  res.send(result);
+  try {
+    const result = await userServices.createAdminIntoDB(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(400).json({
+      success: false,
+      message: err.name || "Admin creation failed!",
+      error: err,
+    });
+  }
 };
 
 // get all admins
 const getAllAdmins: RequestHandler = async (req, res) => {
   const result = await userServices.getAllAdmins();
-  res.send({
+  res.status(200).json({
+    success: true,
+    message: "All Admins fetched successfully!",
     data: result,
   });
 };
