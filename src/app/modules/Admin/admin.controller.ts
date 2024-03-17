95a30b8d871cf383450a7647cda6343296dfcd50
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-17 13:39:29 +0600

    Added Search filtering system

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,8 +3,9 @@ import { adminServices } from "./admin.service";
 
 // get all admins
 const getAllAdmins: RequestHandler = async (req, res) => {
+
   try {
-    const result = await adminServices.getAllAdmins();
+    const result = await adminServices.getAllAdmins(req.query);
     res.status(200).json({
       success: true,
       message: "All Admins fetched successfully!",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,8 +3,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // get all admins from DB
-const getAllAdmins = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdmins = async (params: any) => {
+  console.log(params);
+
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm || "",
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm || "",
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
