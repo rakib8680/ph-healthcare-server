618b7c918344893fd55439a0261e70378cc31658
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-23 17:09:11 +0600

    Add ApiError class and handle unauthorized access in auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,18 +2,21 @@ import { Secret } from "jsonwebtoken";
 import config from "../../config";
 import { verifyToken } from "../../helpers/jwtHelpers";
 import catchAsync from "../../shared/catchAsync";
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status";
 
 const auth = (...roles: string[]) => {
   return catchAsync(async (req, res, next) => {
     // get the token from client
     const token = req.headers.authorization;
-    if (!token) throw new Error("You are not authorized");
+    if (!token)
+      throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
 
     // verify the token
     const decodedData = verifyToken(token, config.jwtAccessSecret as Secret);
 
     if (roles.length && !roles.includes(decodedData.role)) {
-      throw new Error("You are not authorized");
+      throw new ApiError(httpStatus.UNAUTHORIZED,"You are not authorized");
     }
 
     next();
