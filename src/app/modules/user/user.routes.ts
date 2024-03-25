d757ccb6098f38e140182aeb716c82ac680b5574
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-25 14:47:17 +0600

    Add multer for file uploading and update user routes

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,18 +2,14 @@ import express from "express";
 import { userControllers } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import upload from "../../../helpers/fileUploader";
 
 const router = express.Router();
 
 router.post(
   "/create-admin",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
-  userControllers.createAdmin
-);
-
-router.post(
-  "/create-admin",
-  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  upload.single("file"),
   userControllers.createAdmin
 );
 
