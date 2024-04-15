eecb242cdf1acb5689b186d25c4c341e35c066a3
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-15 17:56:46 +0600

    Add appointmentId field to DoctorSchedules model and create relation with Appointment model

--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -1,6 +1,6 @@
 import { JwtPayload } from "jsonwebtoken";
 import prisma from "../../../shared/prisma";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 
 
@@ -8,57 +8,76 @@ import {v4 as uuidv4} from 'uuid';
 const createAppointment = async (user: JwtPayload, payload: any) => {
 
 
-    const patientData = await prisma.patient.findUniqueOrThrow({
-        where: {
-            email: user?.email
-        }
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
+
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+    },
+  });
+
+
+  await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      doctorId: doctorData.id,
+      scheduleId: payload.scheduleId,
+      isBooked: false,
+    },
+  });
+
+
+  const videoCallingId = uuidv4();
+
+
+
+  const result = await prisma.$transaction(async (tx) => {
+
+    const appointmentData = await tx.appointment.create({
+      data: {
+        patientId: patientData.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId,
+      },
+      include: {
+        patient: true,
+        doctor: true,
+        schedule: true,
+      },
     });
 
 
-    const doctorData = await prisma.doctor.findUniqueOrThrow({
-        where: {
-            id: payload.doctorId
-        }
-    });
-
-
-
-    await prisma.doctorSchedules.findFirstOrThrow({
-        where: {
-            doctorId: doctorData.id,
-            scheduleId: payload.scheduleId,
-            isBooked: false
-        }
-    });
-
-
-    const videoCallingId = uuidv4();
-
-    
-    const result = await prisma.appointment.create({
-        data: {
-            patientId: patientData.id,
-            doctorId: doctorData.id,
-            scheduleId: payload.scheduleId,
-            videoCallingId
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: doctorData.id,
+          scheduleId: payload.scheduleId,
         },
-        include: {
-            patient: true,
-            doctor: true,
-            schedule: true
-        }
+      },
+      data: {
+        isBooked: true,
+        appointmentId: appointmentData.id,
+      },
     });
 
 
-    return result;
+    return appointmentData;
 
-}
+  });
 
+
+  return result;
+
+};
 
 
 
 
 
 export const AppointmentService = {
-    createAppointment
-}
+  createAppointment,
+};
