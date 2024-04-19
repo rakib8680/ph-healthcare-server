65f96a6267e7b0f027b1fc9af5d4ebc8f5700e86
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-19 23:19:35 +0600

    Add server exit handlers
    " End of Ph Health Care server "

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,35 @@ import { Server } from "http";
 import app from "./app";
 const port = 3000;
 
+
+
 async function main() {
   const server: Server = app.listen(port, () => {
     console.log("app running on port : ", port);
+  });
+
+  const exitHandler = () => {
+    // close server
+    if (server) {
+      server.close(() => {
+        console.info("server closed");
+      });
+    }
+    process.exit(1);
+  };
+
+  // uncaught exception
+  process.on("uncaughtException", (err) => {
+    console.log("uncaughtException", err);
+
+    exitHandler();
+  });
+
+  // unhandled rejection
+  process.on("unhandledRejection", (err) => {
+    console.log("unhandledRejection", err);
+
+    exitHandler();
   });
 }
 
